a7ac7a35673cb29c001d4aac5a4c36ececfd7685
Karyva <100243636+KarinkaIT13@users.noreply.github.com>
2022-10-16 16:03:25 +0300

    func getLabelText

--- SuperApp/ViewController.swift
+++ SuperApp/ViewController.swift
@@ -24,21 +24,26 @@ class ViewController: UIViewController {
     func startTimer() {
         Timer.scheduledTimer(withTimeInterval:  1, repeats: true) { (_) in
             
-            let date = Date()
-            
-            let dateFormatter = DateFormatter()
-            dateFormatter.dateFormat = "hh:mm:ss"
-            dateFormatter.timeZone = TimeZone(identifier: "Europe/Kyiv")
-            let currentTime = dateFormatter.string(from: date)
-            
-            
-            let dateFormatter2 = DateFormatter()
-            dateFormatter2.dateFormat = "hh:mm:ss"
-            dateFormatter2.timeZone = TimeZone(identifier: "Asia/Bangkok")
-            let currentTime2 = dateFormatter2.string(from: date)
-            
-            self.textLabel.text = "Kyiw:\(currentTime)\nBangkok:\(currentTime2)"
+            self.textLabel.text = self.getLabelText()
         }
-        
     }
+    func getLabelText() -> String {
+        let date = Date()
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "hh:mm:ss"
+        dateFormatter.timeZone = TimeZone(identifier: "Europe/Kyiv")
+        let currentTime = dateFormatter.string(from: date)
+        
+        
+        let dateFormatter2 = DateFormatter()
+        dateFormatter2.dateFormat = "hh:mm:ss"
+        dateFormatter2.timeZone = TimeZone(identifier: "Asia/Bangkok")
+        let currentTime2 = dateFormatter2.string(from: date)
+        
+        self.textLabel.text = "Kyiw:\(currentTime)\nBangkok:\(currentTime2)"
+    
+        return "Kyiw:\(currentTime)\nBangkok:\(currentTime2)"
+    }
+    
 }

5bf55a88143ca835db58bdde80a148b049fdfd39
Karyva <100243636+KarinkaIT13@users.noreply.github.com>
2022-10-26 00:26:22 +0300

    add segment control

--- SuperApp/ViewController.swift
+++ SuperApp/ViewController.swift
@@ -9,25 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var textLabel: UILabel!
-    
-    @IBAction func clickButton(_ sender: Any) {
-        
-    }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.view.backgroundColor = .blue
-        self.startTimer()
-            
-        
-    }
-    
-    func startTimer() {
-        Timer.scheduledTimer(withTimeInterval:  1, repeats: true) { (_) in
-            
-            self.textLabel.text = self.getLabelText()
-        }
-    }
-    func getLabelText() -> String {
+    @IBOutlet weak var segmentControlOutlet: UISegmentedControl!
+   
+    @IBAction func segmentControlAction(_ sender: UISegmentedControl) {
         let date = Date()
         
         let dateFormatter = DateFormatter()
@@ -41,9 +25,27 @@ class ViewController: UIViewController {
         dateFormatter2.timeZone = TimeZone(identifier: "Asia/Bangkok")
         let currentTime2 = dateFormatter2.string(from: date)
         
-        self.textLabel.text = "Kyiw:\(currentTime)\nBangkok:\(currentTime2)"
-    
-        return "Kyiw:\(currentTime)\nBangkok:\(currentTime2)"
+        if sender.selectedSegmentIndex == 0 {
+            self.textLabel.text = "\(currentTime)" }
+        if sender.selectedSegmentIndex == 1 {
+            self.textLabel.text = "\(currentTime2)" }
+        
     }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+        self.startTimer()
+        
+    }
+    
+    func startTimer() {
+        Timer.scheduledTimer(withTimeInterval:  1, repeats: true) { [self] (_) in
+            textLabel.text = self.textLabel.text
+        
+            
+        }
+    }
+    
     
 }
